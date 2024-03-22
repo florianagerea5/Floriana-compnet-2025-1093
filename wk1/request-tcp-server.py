05bd0d370e47b7327e9d666ef57a588a1fab533f
AT <hypothetical.andrei@gmail.com>
2024-03-22 16:39:22 +0200

    .

--- wk1/request-tcp-server.py
+++ wk1/request-tcp-server.py
@@ -1,16 +1,30 @@
 import socketserver
 
-class SimpleTcpHandler(socketserver.BaseRequestHandler):
-  def handle(self):
-    self.data = self.request.recv(1024).strip()
-    print('{} wrote {}'.format(self.client_address[0], self.data))
-    self.response = self.data.upper()
-    self.request.sendall(self.response)
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    """
+    The request handler class for our server.
 
-def main():
-  HOST, PORT = 'localhost', 3333
-  with socketserver.TCPServer((HOST, PORT), SimpleTcpHandler) as server:
-    server.serve_forever()
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
 
-if __name__ == '__main__':
-  main()
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print("{} wrote:".format(self.client_address[0]))
+        print(self.data)
+        # just send back the same data, but upper-cased
+        self.request.sendall(self.data.upper())
+
+class MyTCPServer(socketserver.TCPServer):
+    allow_reuse_address = True
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 12345
+
+    # Create the server, binding to localhost on port 9999
+    with MyTCPServer((HOST, PORT), MyTCPHandler) as server:
+        # Activate the server; this will keep running until you
+        # interrupt the program with Ctrl-C
+        server.serve_forever()
